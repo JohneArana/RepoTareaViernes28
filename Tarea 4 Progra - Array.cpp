fd45fd28fd4174611b04ed61c250b7897cee820f
Johne Arana <jaranar4@miumg.edu.gt>
2025-03-18 23:07:37 -0600

    Se finalizo la tarea 4

--- Tarea 4 Progra - Array.cpp
+++ Tarea 4 Progra - Array.cpp
@@ -71,8 +71,26 @@ main(){
 */
 //Realizar un arreglo de char, es equivalente a realizar un string
 	
-	
-	
-	
+	//ARREGLOS BIDIMENCIONALES
+	int fil=0, col=0;
+	cout<<"ingrese filas: ";
+	cin>>fil;
+	cout<<"ingrese columnas: ";
+	cin>>col;
+	int matriz[fil][col];
+	cout<<" ------------Ingresar----------"<<endl;
+	for(int i=0; i<fil; i++){
+		for(int ii=0; ii<col; ii++){
+			cout<<i<<","<<ii<<": ";
+			cin>>matriz[i][ii];
+		}
+		cout<<"_______________________"<<endl;
+	}
+	cout<<" ------------Mostrar----------"<<endl;
+	for(int i=0; i<fil; i++){
+		for(int ii=0; ii<col; ii++){
+			cout<<i<<","<<ii<<": "<<matriz[i][ii]<<endl;
+		}
+}
 	system("pause");
 }
